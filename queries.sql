b2d8330d00ba6246b76f6cbefc760616633f89c5
Marco Maida <errante@ErranteMachine.homenet.telecomitalia.it>
2018-06-07 19:32:27 +0200

    material

---
+++ queries.sql
@@ -0,0 +1,72 @@
+--Query 1:
+--Contare il numero di lingue in cui le release contenute nel database sono scritte (il risultato deve contenere
+--soltanto il numero delle lingue, rinominato “Numero_Lingue”).
+
+--Query 2:
+--Elencare gli artisti che hanno cantato canzoni in italiano (il risultato deve contenere il nome dell’artista e il nome
+--della lingua).
+
+--Query 3:
+--Elencare le release di cui non si conosce la lingua (il risultato deve contenere soltanto il nome della release).
+
+--Query 4:
+--Elencare gli artisti il cui nome contiene tutte le vocali ed è composto da una sola parola (il risultato deve
+--contenere soltanto il nome dell’artista).
+
+--Query 5:
+--Elencare tutti gli pseudonimi di Prince con il loro tipo, se disponibile (il risultato deve contenere lo pseudonimo
+--dell'artista, il nome dell’artista (cioè Prince) e il tipo di pseudonimo (se disponibile)).
+
+--Query 6:
+--Elencare le release di gruppi inglesi ancora in attività (il risultato deve contenere il nome del gruppo e il nome
+--della release e essere ordinato per nome del gruppo e nome della release)
+
+--Query 7:
+--Trovare le release in cui il nome dell’artista è diverso dal nome accreditato nella release (il risultato deve
+--contenere il nome della release, il nome dell’artista accreditato (cioè artist_credit.name) e il nome dell’artista
+--(cioè artist.name))
+
+--Query 8:
+--Trovare gli artisti con meno di tre release (il risultato deve contenere il nome dell’artista ed il numero di
+--release).
+
+--Query 9:
+--Trovare la registrazione più lunga di un’artista donna (il risultato deve contenere il nome della registrazione, la
+--sua durata in minuti e il nome dell’artista; tenere conto che le durate sono memorizzate in millesimi di secondo)
+--(scrivere due versioni della query con e senza operatore aggregato MAX).
+--07/05/18
+
+--Query 10:
+--Elencare le lingue cui non corrisponde nessuna release (il risultato deve contenere il nome della lingua, il numero
+--di release in quella lingua, cioè 0, e essere ordinato per lingua) (scrivere due versioni della query).
+
+--Query 11:
+--Ricavare la seconda registrazione per lunghezza di un artista uomo (il risultato deve comprendere l'artista
+--accreditato, il nome della registrazione e la sua lunghezza) (scrivere due versioni della query).
+
+--Query 12:
+--Per ogni stato esistente riportare la lunghezza totale delle registrazioni di artisti di quello stato (il risultato deve
+--comprendere il nome dello stato e la lunghezza totale in minuti delle registrazioni (0 se lo stato non ha
+--registrazioni) (scrivere due versioni della query).
+
+--Query 13:
+--Ricavare gli artisti britannici che hanno pubblicato almeno 10 release (il risultato deve contenere il nome
+--dell’artista, il nome dello stato (cioè United Kingdom) e il numero di release) (scrivere due versioni della query).
+
+--Query 14:
+--Considerando il numero medio di tracce tra le release pubblicate su CD, ricavare gli artisti che hanno pubblicato
+--esclusivamente release con più tracce della media (il risultato deve contenere il nome dell’artista e il numero di
+--release ed essere ordinato per numero di release discendente) (scrivere due versioni della query).
+
+--Query 15:
+--Ricavare il primo artista morto dopo Louis Armstrong (il risultato deve contenere il nome dell’artista, la sua data
+--di nascita e la sua data di morte) (scrivere due versioni della query).
+
+--Query 16:
+--Elencare le coppie di etichette discografiche che non hanno mai fatto uscire una release in comune ma hanno fatto
+--uscire una release in collaborazione con una medesima terza etichetta (il risultato deve comprendere i nomi delle
+--coppie di etichette discografiche) (scrivere due versioni della query).
+
+--Query 17 (facoltativa):
+--Trovare il nome e la lunghezza della traccia più lunga appartenente a una release rilasciata in almeno due paesi (il
+--risultato deve contenere il nome della traccia e la sua lunghezza in secondi) (scrivere due versioni della query).
