a63ee689f9f53e6ad16d5ef2403c69a9671f58b5
Marco Perronet <arnad.1997@gmail.com>
2018-06-10 00:03:19 +0200

    more TUPLE

--- queries.sql
+++ queries.sql
@@ -1,4 +1,8 @@
-﻿--Query 1:
+﻿--Pari: io
+--Dispari: tu
+--OCCHIO: se vuoi elencare release senza ripetizioni usa release_group piuttosto che release con la distinct
+
+--Query 1:
 --Contare il numero di lingue in cui le release contenute nel database sono scritte (il risultato deve contenere
 --soltanto il numero delle lingue, rinominato “Numero_Lingue”).
 
@@ -21,7 +25,7 @@ JOIN artist_credit ON track.artist_credit = artist_credit.id
 
 SELECT release.name
 FROM release
-JOIN language ON release.language = language.id AND language.iso_code_1 IS NULL
+JOIN language ON release.language = language.id AND language.iso_code_1 IS NULL --RIVEDERE
 
 --Query 4:
 --Elencare gli artisti il cui nome contiene tutte le vocali ed è composto da una sola parola (il risultato deve
@@ -44,6 +48,14 @@ WHERE 	    (artist.name LIKE '%A%' OR artist.name LIKE '%a%')
 --Elencare le release di gruppi inglesi ancora in attività (il risultato deve contenere il nome del gruppo e il nome
 --della release e essere ordinato per nome del gruppo e nome della release)
 
+SELECT artist.name, release_group.name FROM release_group 
+JOIN artist_credit ON release_group.artist_credit = artist_credit.id
+JOIN artist_credit_name ON artist_credit.id = artist_credit_name.artist_credit
+JOIN artist ON artist.id = artist_credit_name.artist AND artist.ended = FALSE
+JOIN artist_type ON artist.type = artist_type.id AND artist_type.name = 'Group'
+JOIN area ON artist.area = area.id AND area.name = 'United Kingdom' --TODO siamo sicuri che vada bene uk?
+ORDER BY artist.name, release_group.name
+
 --Query 7:
 --Trovare le release in cui il nome dell’artista è diverso dal nome accreditato nella release (il risultato deve
 --contenere il nome della release, il nome dell’artista accreditato (cioè artist_credit.name) e il nome dell’artista
@@ -52,6 +64,13 @@ WHERE 	    (artist.name LIKE '%A%' OR artist.name LIKE '%a%')
 --Query 8:
 --Trovare gli artisti con meno di tre release (il risultato deve contenere il nome dell’artista ed il numero di
 --release).
+
+SELECT artist.name, count(release.id) total_releases FROM release
+JOIN artist_credit ON release.artist_credit = artist_credit.id
+JOIN artist_credit_name ON artist_credit.id = artist_credit_name.artist_credit
+JOIN artist ON artist.id = artist_credit_name.artist
+GROUP BY artist.id
+HAVING count(release.id) < 3
 
 --Query 9:
 --Trovare la registrazione più lunga di un’artista donna (il risultato deve contenere il nome della registrazione, la
@@ -63,6 +82,15 @@ WHERE 	    (artist.name LIKE '%A%' OR artist.name LIKE '%a%')
 --Elencare le lingue cui non corrisponde nessuna release (il risultato deve contenere il nome della lingua, il numero
 --di release in quella lingua, cioè 0, e essere ordinato per lingua) (scrivere due versioni della query).
 
+SELECT * FROM
+(SELECT language.name FROM language
+EXCEPT
+SELECT language.name FROM language
+JOIN release ON release.language = language.id) AS result--TODO MANCA IL COUNT MA CHE CAZZO DI SENSO HA!?!?!
+ORDER BY result.name
+
+--TODO FARE VERSIONE 2 CON LEFT O RIGHT JOIN
+
 --Query 11:
 --Ricavare la seconda registrazione per lunghezza di un artista uomo (il risultato deve comprendere l'artista
 --accreditato, il nome della registrazione e la sua lunghezza) (scrivere due versioni della query).
@@ -71,6 +99,19 @@ WHERE 	    (artist.name LIKE '%A%' OR artist.name LIKE '%a%')
 --Per ogni stato esistente riportare la lunghezza totale delle registrazioni di artisti di quello stato (il risultato deve
 --comprendere il nome dello stato e la lunghezza totale in minuti delle registrazioni (0 se lo stato non ha
 --registrazioni) (scrivere due versioni della query).
+
+SELECT area.name, sum(recording.length) total_length FROM area
+JOIN area_type ON area.type = area_type.id AND area_type.name = 'Country'
+JOIN artist ON artist.area = area.id
+JOIN artist_credit_name ON artist_credit_name.artist = artist.id
+JOIN artist_credit ON artist_credit.id = artist_credit_name.artist_credit
+JOIN recording ON artist_credit.id = recording.artist_credit --nemmeno facendo LEFT JOIN qui stampa gli zeri!
+GROUP BY area.id
+
+--Test left join (funziona ma comunque niente zeri)
+SELECT artist_credit.name, sum(length) FROM artist_credit LEFT JOIN recording ON artist_credit.id = recording.artist_credit GROUP BY artist_credit.id
+
+--TODO SECONDA VERSIONE
 
 --Query 13:
 --Ricavare gli artisti britannici che hanno pubblicato almeno 10 release (il risultato deve contenere il nome
